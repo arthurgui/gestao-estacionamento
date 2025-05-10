26cdacffde416ec1eac9847b512e28a89e8261a1
INDRA\apereirag <arthur.guimaraes.external@enel.com>
2025-05-10 10:20:59 -0300

    fazendo teste de postamn e conexcao com o banco de dados postgre

--- src/main/java/com/mazza/tech/gestao/estacionamento/controller/GarageController.java
+++ src/main/java/com/mazza/tech/gestao/estacionamento/controller/GarageController.java
@@ -3,6 +3,7 @@ package com.mazza.tech.gestao.estacionamento.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
